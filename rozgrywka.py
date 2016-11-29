a77f1908c658706199d4e4c103e968186dec338c
Lukasz Cieply <hurricane.pl.96@gmail.com>
2016-11-29 21:30:41 +0100

    rozdzielono Pionek na ProstyPionek i Pionek

--- rozgrywka.py
+++ rozgrywka.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
 from tools import *
 import pygame
-import os
-import sys
-from pygame.locals import *
 
 
 
@@ -17,22 +14,23 @@ class Rozgrywka:
         self.pionki = list()  # lista zawierajaca wszystkie pionki
 
         for foo in range(0, 8, 2):  # dodaj pionki
-            self.pionki.append(Pionek((self.rect.width/8, self.rect.height/8), (foo, 7), 0))  # czarne
+            self.pionki.append(Pionek((self.rect.width/8, self.rect.height/8), (foo, 7), 0))  # czarne - dol ekranu
             self.pionki.append(Pionek((self.rect.width/8, self.rect.height/8), (foo+1, 6), 0))
 
             self.pionki.append(Pionek((self.rect.width/8, self.rect.height/8), (foo, 1), 1))  # biale
             self.pionki.append(Pionek((self.rect.width/8, self.rect.height/8), (foo+1, 0), 1))
 
-        self.lista_pionkow = pygame.sprite.RenderPlain(self.pionki)  # Uchwyt slużący do renderowania pionków
+        self.renderuj_pionki = pygame.sprite.RenderPlain(self.pionki)  # Uchwyt slużący do renderowania pionków
 
         self.przenoszenie = -1
 
     def update(self):
-        self.lista_pionkow.update()
-        self.lista_pionkow.draw(self.screen)
+        self.renderuj_pionki.update()
+        self.renderuj_pionki.draw(self.screen)
 
     def click(self, pos):
         debug("[klik]: ", pos)
+
         if self.przenoszenie != -1:  # JEST w trybie przenoszenia
             debug("[klik]: odlozenie!")
             self.screen.blit(self.image, self.przenoszenie.rect, self.przenoszenie.rect)
@@ -48,30 +46,68 @@ class Rozgrywka:
 
     def przesun_pionek(self, pionek, x, y):
         ox, oy = pionek.getpos()
+
         if pionek.color == 0:  # jesli pionek jest czarny
             if oy == y+1 and (ox == x-1 or ox == x+1):
                 pionek.move(x, y)
+                debug(mozliwe_ruchy(pionek, self.pionki))
+
         elif pionek.color == 1:
             if oy == y-1 and (ox == x-1 or ox == x+1):
                 pionek.move(x, y)
 
 
-class Pionek(pygame.sprite.Sprite):
+def czy_jest_na_polu(x, y, pionki):
+    if x > 7 or y > 7 or x < 0 or y < 0:
+        return -1
+    for foo in pionki:
+        if foo.getpos() == (x, y):
+            return 1
+    return 0
+
+
+def mozliwe_ruchy(pionek, arg_pionki):  # mozliwe ruchy dla pionka z dana liczba pionkow
+    kopia_pionki = list(arg_pionki)
+    kopia_pionki.remove(pionek)
+
+    zwracana_lista = list()
+    pos = pionek.getpos()
+    debug("pozycja", pos)
+    if czy_jest_na_polu(pos[0]-1, pos[1]-1, kopia_pionki) == 1 and czy_jest_na_polu(pos[0]-2, pos[1]-2, kopia_pionki) == 0:
+        # Przeciwnik na lewo do przodu i wolne miejsce za nim: mozna bic!
+
+        debug("bij lewaka")
+        zwracana_lista.append((pos[0]-2, pos[1]-2)) == 1
+    if czy_jest_na_polu(pos[0]+1, pos[1]-1, kopia_pionki) and czy_jest_na_polu(pos[0]+2, pos[1]-2, kopia_pionki) == 0:
+        # Przeciwnik na prawo do przodu i wolne miejsce za nim: mozna bic!
+        zwracana_lista.append((pos[0]+2, pos[1]-2))
+        debug("bij prawaka")
+    return zwracana_lista
+
+class ProstyPionek:
+    def __init__(self, cords, color):
+        self.color = color
+        self.cords = cords
+
+    def move(self, x, y):
+        self.cords = (x, y)
+
+
+class Pionek(ProstyPionek, pygame.sprite.Sprite):
     def __init__(self,  size, cords, color): # color 0-black 1-white
         pygame.sprite.Sprite.__init__(self)
+        ProstyPionek.__init__(self, cords, color)
+        self.size = size
 
-        self.image, self.rect = load_png("pionek.png")
-        self.color = color
+        self.image, rect = load_png("pionek.png")
 
         # Skaluj pionek
         self.image = pygame.transform.scale(self.image, size)
-        self.rect.width, self.rect.height = size
-        self.rect.width
         self.move(*cords)
 
-    def move(self, x, y):
-        self.rect.x = x * self.rect.width
-        self.rect.y = y * self.rect.height
+    @property
+    def rect(self):
+        return pygame.Rect((self.cords[0]*self.size[0], self.cords[1]*self.size[1]), self.size)
 
     def getpos(self):
         return self.rect.x/self.rect.width, self.rect.y/self.rect.height
