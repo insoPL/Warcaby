773c4b7e04cd23f73d497216c2befbc7f9117897
Lukasz Cieply <hurricane.pl.96@gmail.com>
2016-11-30 16:32:54 +0100

    oznaczenie oraaz pionek przeniesione do osobnego pliku

---
+++ oznaczenie.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from tools import *
+import pygame
+
+
+class Oznaczenie(pygame.sprite.Sprite):
+    def __init__(self, size, cords):
+        pygame.sprite.Sprite.__init__(self)
+        self.cords = cords
+        self.size = size
+
+        self.image, rect = load_png("oznaczenie.png")
+
+        # Skaluj oznaczenie
+        self.image = pygame.transform.scale(self.image, size)
+
+        self.move(*cords)
+
+    def move(self, x, y):
+        self.cords = (x, y)
+
+    @property
+    def rect(self):
+        return pygame.Rect((self.cords[0] * self.size[0], (7 - self.cords[1]) * self.size[1]), self.size)
